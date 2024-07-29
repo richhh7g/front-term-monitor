71992d90f445c664d58ea4c759c3a8cf1939d212
richhh7g <richhh7g@protonmail.com>
2024-07-29 19:11:58 -0300

    feat: add button atom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@atomic/atm.button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Button>Hello World</Button>;
 }
