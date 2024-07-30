fee533dd52d327d4c8c382fd9917df1d39c5c1f2
richhh7g <richhh7g@protonmail.com>
2024-07-29 22:41:17 -0300

    chore: add cmdk config

---
+++ src/atomic/mol.command/index.ts
@@ -0,0 +1,16 @@
+import type {
+  CommandProps,
+  CommandListElement,
+  CommandListProps,
+  CommandItemElement,
+  CommandItemProps,
+} from "./components";
+
+export type {
+  CommandProps,
+  CommandListElement,
+  CommandListProps,
+  CommandItemElement,
+  CommandItemProps,
+};
+export * from "./command.component";
